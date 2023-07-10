a595277a0e254a3d7056ddc1dd1d460301415046
Ethan <ethan.nicolle@icloud.com>
2023-07-10 06:14:00 +0200

    Major changes for language system

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+if (!localStorage.getItem('lang')) {
+    localStorage.setItem('lang', navigator.language)
+}
+
+let langs = {
+    'en': {
+        lang: 'EN',
+        'haut': 'Tops',
+        'bas': 'Bottoms',
+        'chapeau': 'Hats'
+    },
+    'fr': {
+        lang: 'FR',
+        'haut': 'Haut',
+        'bas': 'Bas',
+        'chapeau': 'Chapeau'
+    }
+}
+
+const menuToggler = document.querySelector('.toggle-menu'),
+    langSelector = document.querySelector('.sub-menu'),
+    translate = document.querySelectorAll('#translate');
+
+menuToggler.addEventListener('click', (event) => {
+    langSelector.classList.toggle('sub-menu-show');
+})
+
+const french = document.querySelector('.french'),
+    english = document.querySelector('.english')
+
+french.addEventListener('click', () => {
+    localStorage.setItem('lang', 'fr')
+    location.reload()
+})
+
+english.addEventListener('click', () => {
+    localStorage.setItem('lang', 'en')
+    location.reload()
+})
+
+if (localStorage.getItem('lang') === 'fr') {
+    for (let lang in langs.fr) {
+        translate.forEach((translation) => {
+            if (translation.dataset.type === lang) {
+                translation.textContent = langs.fr[lang]
+            }
+        })
+    }
+} else {
+    for (let lang in langs.en) {
+        translate.forEach((translation) => {
+            if (translation.dataset.type === lang) {
+                translation.textContent = langs.en[lang]
+            }
+        })
+    }
+}
+
 const hamburger = document.querySelector('.hamburger-container'),
     menu = document.querySelector('.menu'),
     rightMenu = document.querySelector('.right-menu'),
@@ -6,10 +64,10 @@ const hamburger = document.querySelector('.hamburger-container'),
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('show');
     rightMenu.classList.toggle('show');
-    if(menu.classList.contains('show')) {
-        if(window.innerWidth < 791){
+    if (menu.classList.contains('show')) {
+        if (window.innerWidth < 791) {
             header.style.height = '100svh';
-        }else {
+        } else {
             header.style.height = '100px';
         }
     } else {
@@ -18,8 +76,8 @@ hamburger.addEventListener('click', () => {
 })
 
 addEventListener('resize', () => {
-    if(menu.classList.contains('show')) {
-        if(window.innerWidth < 791){
+    if (menu.classList.contains('show')) {
+        if (window.innerWidth < 791) {
             header.style.height = '100svh';
         } else {
             menu.classList.toggle('show');
@@ -30,3 +88,4 @@ addEventListener('resize', () => {
         header.style.height = '100px';
     }
 })
+
